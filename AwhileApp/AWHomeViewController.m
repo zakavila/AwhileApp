a06b638aa3b6d144c7d189bd62d8f2cbbc8074ef
Troy Chmieleski <tchmieleski@incubate.co>
2014-04-16 21:16:49 -0500

    lazily load main view.

--- AwhileApp/AWHomeViewController.m
+++ AwhileApp/AWHomeViewController.m
@@ -13,33 +13,42 @@
 #import "AWMilestonesViewController.h"
 
 @interface AWHomeViewController ()
+
 @property (nonatomic, strong) AWHomeView *mainView;
+
 @end
 
 @implementation AWHomeViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    self.mainView = [[AWHomeView alloc] initWithFrame:self.view.frame];
-    [self.mainView.yourAgeButton addTarget:self action:sel_registerName("yourAgeButtonPressed") forControlEvents:UIControlEventTouchUpInside];
-    [self.mainView.calculatorButton addTarget:self action:sel_registerName("calculatorButtonPressed") forControlEvents:UIControlEventTouchUpInside];
-    [self.mainView.milestonesButton addTarget:self action:sel_registerName("milestonesButtonPressed") forControlEvents:UIControlEventTouchUpInside];
+	
     [self.view addSubview:self.mainView];
 }
 
-- (void)yourAgeButtonPressed
-{
+- (void)yourAgeButtonPressed {
     [self.navigationController pushViewController:[[AWYouAreViewController alloc] init] animated:YES];
 }
 
-- (void)calculatorButtonPressed
-{
+- (void)calculatorButtonPressed {
     [self.navigationController pushViewController:[[AWYoullBeViewController alloc] init] animated:YES];
 }
-- (void)milestonesButtonPressed
-{
+
+- (void)milestonesButtonPressed {
     [self.navigationController pushViewController:[[AWMilestonesViewController alloc] init] animated:YES];
+}
+
+#pragma mark - Main view
+
+- (AWHomeView *)mainView {
+	if (!_mainView) {
+		_mainView = [[AWHomeView alloc] initWithFrame:self.view.frame];
+		[_mainView.yourAgeButton addTarget:self action:sel_registerName("yourAgeButtonPressed") forControlEvents:UIControlEventTouchUpInside];
+		[_mainView.calculatorButton addTarget:self action:sel_registerName("calculatorButtonPressed") forControlEvents:UIControlEventTouchUpInside];
+		[_mainView.milestonesButton addTarget:self action:sel_registerName("milestonesButtonPressed") forControlEvents:UIControlEventTouchUpInside];
+	}
+	
+	return _mainView;
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,16 +56,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
