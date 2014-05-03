850ed45e0dc1e537b8c30ecde93b929dcf066dbb
Zak Avila <zak.avila@hotmail.com>
2014-05-03 17:35:47 -0500

    Not needed after start index fixed

--- AwhileApp/AWBirthDateView.m
+++ AwhileApp/AWBirthDateView.m
@@ -306,7 +306,7 @@ typedef NS_ENUM(NSInteger, CircleType) {
         _yearSpinner = [[ZASpinnerView alloc] initWithFrame:CGRectZero];
         _yearSpinner.spinnerDelegate = self;
         _yearSpinner.spinnerType = InfiniteCountSpinner;
-        _yearSpinner.startIndex = [components year] + 75;
+        _yearSpinner.startIndex = [components year];
         _yearSpinner.spinnerName = @"yearSpinner";
         [_yearSpinner registerClass:[AWArcTextSpinnerCell class] forCellReuseIdentifier:ARCTEXT_SPINNER_CELL_IDENTIFIER];
     }
